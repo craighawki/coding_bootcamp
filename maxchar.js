3736d05af48c307262711c57db50deaeee9dbf69
Hawkins, Craig <craig.A.hawkins@ge.com>
2021-01-06 21:53:44 -0800

    reverse maxChar

---
+++ maxchar.js
@@ -0,0 +1,15 @@
+function maxChar(str) {
+    const charMap = {};
+
+    for (let char of str) {
+        if (charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+
+    console.log(charMap);
+
+}
+maxChar('mississippi');
