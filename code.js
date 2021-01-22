7006e4405e87cc975624d499070cc09c30ef5af3
Hawkins, Craig <craig.A.hawkins@ge.com>
2021-01-21 21:56:46 -0800

    anagrams

--- code.js
+++ code.js
@@ -1,14 +1,10 @@
 function chunk(array, size) {
     const chunked = [];
+    let index = 0;
 
-    for (let element of array) {
-        const last = chunked[chunked.length - 1];
-
-        if (!last || last.length === size) {
-            chunked.push([element]);
-        } else {
-            last.push(element);
-        }
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
     }
     return chunked;
 }
