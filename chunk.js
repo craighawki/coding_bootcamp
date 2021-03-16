1313388964ffb8a2802a227496bf611080e58d69
Hawkins, Craig <craig.A.hawkins@ge.com>
2021-03-15 22:02:04 -0700

    added some code

--- chunk.js
+++ chunk.js
@@ -12,4 +12,4 @@ function chunk(array, size) {
     }
     return chunked;
 }
-console.log(chunk([1,2,3,4,5,6,7,8,9,0], 2))
+console.log(chunk([1,2,3,4,5], 2));
--- code.js
+++ code.js
@@ -1,25 +1,14 @@
-function anagrams(stringA, stringB) {
-    const aCharMap = buildCharMap(stringA);
-    const bCharMap = buildCharMap(stringB);
-
-    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-        return false;
-    }
-
-    for (let char in aCharMap) {
-        if (aCharMap[char] !== bCharMap[char]) {
-            return false;
+function fizzbuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz');
+        } else if (i % 3 === 0) {
+            console.log('fizz');
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
         }
     }
-    return true;
 }
-
-function buildCharMap(str) {
-    const charMap = {};
-
-    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-        charMap[char] = charMap[char] + 1 || 1;
-    }
-    return charMap;
-}
-console.log(anagrams('ample', 'maple'));
+console.log(fizzbuzz(15));
